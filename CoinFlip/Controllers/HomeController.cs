632be4d53ae5541b57c80fa95d96b9ff6212f46f
John Korsnes <johnkors@gmail.com>
2012-05-31 13:11:43 +0200

    Made a sln out if it

---
+++ CoinFlip/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace CoinFlip
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+   
+    }
+}
---
+++ CoinFlip/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoinFlip.MvcApplication" Language="C#" %>
